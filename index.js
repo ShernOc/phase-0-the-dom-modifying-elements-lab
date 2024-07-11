bc9e06f8fbfd877e31d58754c484121526a764dd
Sherlyne <sherlynea1994@hotmail.com>
2024-07-11 12:10:37 +0300

    dom modified element

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const main = document.querySelector("#main"); 
+main.remove();
+console.log(main);
+const newHeader = document.createElement("h1"); 
+//adding an Id to an exisiting element. 
+
+// const newHeaderid = document.getElementsByTagName("h1"); 
+// use inner.HTML 
+newHeader.id= "victory" ; 
+
+// Add text to h1 element (element.innerHTML or element.textContent = "Sherlyne is the champion")
+newHeader.textContent = "Sherlyne is the champion"; 
+
+console.log(newHeader);
